55f74350d44e970ecd0d9ac2273e80d43aabbe53
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-10-02 17:40:39 +0800

    Update Ex3_26.cpp

--- ch03/Ex3_26.cpp
+++ ch03/Ex3_26.cpp
@@ -8,12 +8,22 @@ using std::string;
 
 int main(void)
 {
-
+    /***for***/
     string str;
     getline(cin, str);
     for(auto &ch:str)
         ch = 'X';
     cout << str << endl;
-
+    
+    /***while***/
+    string s;
+    getline(cin, s);
+    decltype(s.size()) i = 0;
+    while (i < s.size())
+    {
+        s[i] = 'X';
+        i++;
+    }
+    cout << s << endl;
     return 0;
 }
