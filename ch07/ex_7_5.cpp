02024f20a6911e6dfb57e2d031548fb4690ed5ee
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-10-08 22:14:47 +0800

    Create ex_7_5.cpp

---
+++ ch07/ex_7_5.cpp
@@ -0,0 +1,24 @@
+/***ex_7_5***/
+#include <iostream>
+
+class Person
+{
+private:
+    string name;
+    string address;
+
+public:
+    string getname() const
+    {
+        return name;
+    }
+
+    string getaddress() const
+    {
+        return address;
+    }
+    
+} int main()
+{
+    return 0;
+}
