2393f46a008e1227d112d45979ee8a8fe288c5c3
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-10-13 17:26:49 +0800

    Create ex_10_9.cpp

---
+++ ch10/ex_10_9.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
+void outputWords(vector<string> &v)
+{
+    for (auto it = v.cbegin(); it != v.cend(); ++it)
+        cout << *it << " ";
+    cout << endl;
+}
+
+void elimDups(vector<string> &v)
+{
+    outputWords(v);
+
+    sort(v.begin(), v.end());
+    outputWords(v);
+
+    auto endUnique = unique(v.begin(), v.end());
+    outputWords(v);
+
+    v.erase(endUnique, v.end());
+    outputWords(v);
+}
+int main(int argc, char *argv[])
+{
+    ifstream in(argv[1]);
+    if (!in)
+    {
+        cout << "打开文件夹失败！" << endl;
+        exit(1);
+    }
+
+    vector<string> words;
+    string word;
+    while (in >> word)
+        words.pop_back(word);
+    elimDups(words);
+
+    return 0;
+}
