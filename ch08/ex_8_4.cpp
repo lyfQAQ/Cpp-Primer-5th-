625689773ddde71be815ce92be3f8ff011603ce3
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-12-04 15:10:13 +0800

    Create ex_8_4.cpp

---
+++ ch08/ex_8_4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void Fuck(string const& fileName, vector<string>& buffer)
+{
+	ifstream ifs(fileName);
+	string str;
+	while (getline(ifs, str))
+	{
+		buffer.push_back(str);
+	}
+}
+
+int main()
+{
+	vector<string> buffer;
+
+	Fuck("C:/Users/0219l/Desktop/test.txt", buffer);
+	for (auto str : buffer)
+		cout << str << " ";
+	return 0;
+}
