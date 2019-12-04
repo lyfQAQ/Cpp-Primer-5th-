4174734a0c4de5b49ddaf9a41b05e0cb0fb9439f
Yifan Liu <43963430+vlyf@users.noreply.github.com>
2019-12-04 15:00:44 +0800

    Create ex_8_1.cpp

---
+++ chp08/ex_8_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+istream& Fuck(istream& in)
+{
+	in.clear();
+	string buffer;
+	while (in >> buffer)
+	{
+		cout << buffer;
+	}
+	in.clear();
+	return in;
+}
+
+int main()
+{
+	Fuck(cin);
+	return 0;
+}
